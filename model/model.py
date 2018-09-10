7ea34a7f99f7bb6e5acdacfadc847ed2911fe527
EthanBraun <ebacdc3@gmail.com>
2018-09-10 12:31:57 -0700

    Added pseudocode

--- model/model.py
+++ model/model.py
@@ -1,3 +1,20 @@
 import pandas as pd
+import numpy as np
+from imblearn.over_sampling import ADASYN
 
+trainSplit = 0.9
+
+# Create dataframe from csv
 data = pd.read_csv('../data/data.csv')
+
+# Shuffle data
+data = data.reindex(np.random.permutation(data.index))
+
+# Split into train and test sets
+splitIdx = int(len(data) * trainSplit)
+train, test = data[:splitIdx], data[splitIdx:]
+
+
+
+#y = data.diagnosis
+#x = data.drop(['id', 'diagnosis', 'Unnamed: 32'], axis=1)
